f1c6ce732d5f995c3658f2cc28676cef3b71434b
Dorsa <dorsa.nasimi22@gmail.com>
2024-07-30 11:35:36 +0100

    Initial Commit

---
+++ quiz/quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizApp.swift
+//  quiz
+//
+//  Created by Scholar on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
